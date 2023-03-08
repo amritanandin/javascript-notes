ecfc2630718ced58c87cff6a400468c941e5561f
amritanandin <contact@amritanand.in>
2023-03-08 21:42:02 +0530

    Javascript Comment

--- 002-javascript-comment.js
+++ 002-javascript-comment.js
@@ -7,26 +7,39 @@
 // Single line comments start with //.
 // Any text between // and the end of the line will be ignored by JavaScript (will not be executed).
 
-// Example 1
+
+// Example 1 ****************
+
 // hello world program
 console.log("Ex 1 : Hello world")
+
+// -----------------------------------------------
 // Output :
 // Ex 3 : Hello world
+// -----------------------------------------------
 
-// Example 2
+
+// Example 2 ****************
+
 console.log("Ex 2 : Hello world") // hello world program
+
+// -----------------------------------------------
 // Output :
 // Ex 2 : Hello world
-
+// -----------------------------------------------
 
 
 // ********************  multi line comment  ******************************
 // Multi-line comments start with /* and end with */.
 // Any text between /* and */ will be ignored by JavaScript.
 
-// Example 3
+// Example 3 **********
+
 /* hello world program 
 in javascript */
 console.log("Ex 3 : Hello world")
+
+// -----------------------------------------------
 // Output :
 // Ex 3 : Hello world
+// -----------------------------------------------

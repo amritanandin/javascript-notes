dfb67cb964ea003e9c55370ea97af505928014c3
amritanandin <contact@amritanand.in>
2023-03-08 21:36:34 +0530

    Javascript Comment

---
+++ 001-introduction-of-javascript.js
@@ -0,0 +1,14 @@
+// Introduction :
+// JavaScript is a programming language used primarily for creating interactive and dynamic web content.
+// It was created in 1995 by Brendan Eich, while he was working at Netscape Communications Corporation.
+// JavaScript is a high-level, interpreted language, which means that it is designed to be easy to
+// read and write, and it doesn't need to be compiled before it can be run.
+
+// JavaScript is also used for server-side programming, most commonly through the Node.js 
+// runtime environment. With Node.js, JavaScript can be used to create web servers, command-line tools,
+// and other back-end applications.
+
+// After HTML and CSS, JavaScript is the third biggest web technology. 
+
+// Created In : 1995
+// By         : Brendan Eich
--- spread-operator.js
+++ spread-operator.js
@@ -12,3 +12,8 @@ const numbers = [1, 2, 3, 4, 5, 6];
 const [one, two, ...rest] = numbers;
 console.log(one,two,rest)
 // output: 1 2 [ 3, 4, 5, 6 ]
+
+
+
+// interview question 
+// Q: what is spread operator?
